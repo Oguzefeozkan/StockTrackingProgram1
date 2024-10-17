0d6fc8f65640484d59b9ea030ec3eefadb39de22
Oguzefeozkan <oguzefe29ozkan@gmail.com>
2024-10-17 16:53:28 +0300

    Add files via upload

---
+++ 1713_StockTakipProgramı/1713_StockTakipProgramı/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace _1713_StockTakipProgramı
+{
+    partial class frmKategoriIslemleri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grdKategoriler = new System.Windows.Forms.DataGridView();
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.txtKategoriAdi = new System.Windows.Forms.TextBox();
+            this.text = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdKategoriler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.grdKategoriler);
+            this.groupBox1.Controls.Add(this.btnYeni);
+            this.groupBox1.Controls.Add(this.btnGuncelle);
+            this.groupBox1.Controls.Add(this.btnSil);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.txtKategoriAdi);
+            this.groupBox1.Controls.Add(this.text);
+            this.groupBox1.Location = new System.Drawing.Point(42, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(450, 265);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgi Girişi";
+            // 
+            // grdKategoriler
+            // 
+            this.grdKategoriler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdKategoriler.Location = new System.Drawing.Point(6, 175);
+            this.grdKategoriler.MultiSelect = false;
+            this.grdKategoriler.Name = "grdKategoriler";
+            this.grdKategoriler.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdKategoriler.Size = new System.Drawing.Size(444, 84);
+            this.grdKategoriler.TabIndex = 1;
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.Image = global::_1713_StockTakipProgramı.Properties.Resources.artı;
+            this.btnYeni.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnYeni.Location = new System.Drawing.Point(355, 9);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(89, 44);
+            this.btnYeni.TabIndex = 5;
+            this.btnYeni.Text = "Yeni Kayıt";
+            this.btnYeni.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnYeni.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Image = global::_1713_StockTakipProgramı.Properties.Resources.restart;
+            this.btnGuncelle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGuncelle.Location = new System.Drawing.Point(300, 119);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(102, 49);
+            this.btnGuncelle.TabIndex = 4;
+            this.btnGuncelle.Text = "Güncelleme";
+            this.btnGuncelle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Image = global::_1713_StockTakipProgramı.Properties.Resources.red;
+            this.btnSil.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSil.Location = new System.Drawing.Point(195, 119);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(89, 49);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil      ";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Image = global::_1713_StockTakipProgramı.Properties.Resources.artı;
+            this.btnEkle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEkle.Location = new System.Drawing.Point(76, 119);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(91, 49);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            // 
+            // txtKategoriAdi
+            // 
+            this.txtKategoriAdi.Location = new System.Drawing.Point(186, 53);
+            this.txtKategoriAdi.Name = "txtKategoriAdi";
+            this.txtKategoriAdi.Size = new System.Drawing.Size(130, 20);
+            this.txtKategoriAdi.TabIndex = 1;
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Location = new System.Drawing.Point(84, 56);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(67, 13);
+            this.text.TabIndex = 0;
+            this.text.Text = "Katogeri Adı:";
+            // 
+            // frmKategoriIslemleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 348);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmKategoriIslemleri";
+            this.Text = "KatogoriIsemleri";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdKategoriler)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtKategoriAdi;
+        private System.Windows.Forms.Label text;
+        private System.Windows.Forms.DataGridView grdKategoriler;
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ 1713_StockTakipProgramı/1713_StockTakipProgramı/Form6.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace _1713_StockTakipProgramı
+{
+    partial class frmGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PasswordChar = new System.Windows.Forms.PictureBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PasswordChar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(276, 45);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(130, 20);
+            this.txtKullaniciAdi.TabIndex = 44;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(206, 48);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(67, 13);
+            this.label.TabIndex = 43;
+            this.label.Text = "Kullanıcı Adı:";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(276, 93);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(130, 20);
+            this.txtSifre.TabIndex = 46;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Şifre:";
+            // 
+            // PasswordChar
+            // 
+            this.PasswordChar.Image = global::_1713_StockTakipProgramı.Properties.Resources.Ekran_görüntüsü_2024_10_10_100010;
+            this.PasswordChar.Location = new System.Drawing.Point(32, 29);
+            this.PasswordChar.Name = "PasswordChar";
+            this.PasswordChar.Size = new System.Drawing.Size(143, 144);
+            this.PasswordChar.TabIndex = 48;
+            this.PasswordChar.TabStop = false;
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Image = global::_1713_StockTakipProgramı.Properties.Resources.resim;
+            this.btnGiris.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGiris.Location = new System.Drawing.Point(276, 138);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(130, 35);
+            this.btnGiris.TabIndex = 47;
+            this.btnGiris.Text = "Giriş Yap";
+            this.btnGiris.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGiris.UseVisualStyleBackColor = true;
+            // 
+            // frmGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 203);
+            this.Controls.Add(this.PasswordChar);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.label);
+            this.Name = "frmGiris";
+            this.Text = "Form6";
+            ((System.ComponentModel.ISupportInitialize)(this.PasswordChar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.PictureBox PasswordChar;
+    }
+}
